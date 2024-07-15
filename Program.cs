da6fa881847b32e283718e77c7d03de3820a14aa
alexk <alex.krasnov42@gmail.com>
2024-07-15 12:39:36 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using currency_rate;
+using System.Text;
+
+namespace CurrencyLoader
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (var _context = new CurrencyRateContext())
+            {
+                _context.Database.EnsureCreated();
+            }
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            var service = new CurrencyService();
+
+            await service.DailyAsync();
+
+            await service.FillRatesForLastMonthAsync();
+
+        }
+    }
+}
